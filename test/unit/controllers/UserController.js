61ae9aad1a91807ffcdb927a5180dba30c6175e0
Tristan Foureur <tristan.foureur@gmail.com>
2015-04-18 17:05:01 +0530

    Add pending tests for UserController

---
+++ test/unit/controllers/UserController.js
@@ -0,0 +1,10 @@
+var request = require('supertest');
+var user1;
+
+describe('UserController', function() {
+  it.skip("should require is_admin for CRUD methods");
+  describe("#edit", function() {
+    it.skip("should allow the user to edit his profile");
+  });
+
+});
